f45ca6bd85389f55984d50769adc941949815f81
Pandolphi <pandolphi@gmail.com>
2025-04-09 12:28:18 -0300

    ok

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: "zabbix-whatsapp-ia",
+    script: "./src/server.js",
+    watch: false,
+    env: {
+      NODE_ENV: "production",
+      PORT: 3001
+    },
+    instances: 1,
+    autorestart: true,
+    max_memory_restart: "1G",
+    error_file: "logs/err.log",
+    out_file: "logs/out.log",
+    time: true
+  }]
+}; 
